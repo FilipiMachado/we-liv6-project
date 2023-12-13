34bfd553f11d7bb54b18e3df1a4ea162f2ef81c3
filipi <filipisee@hotmail.com>
2023-12-12 21:05:03 -0300

    reworked authentication pages

--- app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-  return (
-    <div>
-      <div>Login Page</div>
-    </div>
-  );
-};
-
-export default LoginPage;
--- app/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-  return (
-    <div>
-      <div>Register Page</div>
-    </div>
-  );
-};
-
-export default RegisterPage;
