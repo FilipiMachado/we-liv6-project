0f60a71a29bbe2695456c7d45d0665aa3a262fb3
filipi <filipisee@hotmail.com>
2023-12-14 13:53:56 -0300

    structuring search input

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,9 +1,31 @@
-"use client"
+"use client";
+
+import qs from "query-string";
+import { useState } from "react";
+import { SearchIcon, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const Search = () => {
-    return (
-        <div>
-            Search!
-        </div>
-    )
-}
+  return (
+    <form
+      onSubmit={() => {}}
+      className="relative w-full lg:w-[400px] flex items-center"
+    >
+      <Input
+        placeholder="Search"
+        className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+      />
+      <Button
+        type="submit"
+        size="sm"
+        variant="secondary"
+        className="rounded-l-none"
+      >
+        <SearchIcon className="w-5 h-5 text-muted-foreground" />
+      </Button>
+    </form>
+  );
+};
