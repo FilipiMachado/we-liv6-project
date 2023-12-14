e999aab1009cd2e26cc1a5faa688635cb31c16a2
filipi <filipisee@hotmail.com>
2023-12-14 11:29:12 -0300

    structurng browse/home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
       <h1>Dashboard</h1>
-      <UserButton afterSignOutUrl="/"/>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
